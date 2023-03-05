3742db7cf60cff2904b88ada957a096c16331c51
javad pournosrat <pournosrat.javad@gmail.com>
2023-03-05 21:42:25 +0330

    add plot

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 # X = number of impact
 # Y = Moisture percentage
 
@@ -14,14 +15,23 @@ k = Y2 - Y1
 
 
 m = k/j
-print('m:' + str(m))
+#print('m:' + str(m))
 
 
-def y(x):
+def f(x):
     f = x-X1
     h = m*f
+    global y
     y = h+Y1
     #y = m*(x-X1) - Y1
     print(y)
 
-y(25)
+f(25)
+X = sorted([25, X1, X2])
+Y = [Y1, Y2, y]
+print(X)
+print(Y)
+
+fig, ax = plt.subplots()
+ax.plot(X, Y)
+plt.show()
